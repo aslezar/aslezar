0b29d5e78a55ebc145c375d06dbd26506a7adc97
aslezar <97354675+aslezar@users.noreply.github.com>
2024-02-12 00:11:14 +0530

    fixed

--- src/ExperienceList.jsx
+++ src/ExperienceList.jsx
@@ -1,32 +1,32 @@
 export default [
-  {
-    title: "Vetmedman",
-    img: "https://i.imgur.com/UTZMoZt.jpg",
-    techStack: ["MongoDB", "NodeJS", "ExpressJS", "ReactJS"],
-    time_from: "Sep 2023",
-    time_to: "Feb 2024",
-    description:
-      "I developed Vetmedman's admin panel from the ground up using the MERN stack. This platform allows easy management of user data, articles, banners, orders, inquiries, and order statuses. It enhances user experience within the veterinary medical equipment and supplies sector..",
-    liveDemo: "https://www.vetmedman.com/",
-    // githubRepo: "https://github.com/aslezar/aslezar",
-  },
-  {
-    title: "ShareSketch",
-    img: "https://github.com/aslezar/ShareSketch/raw/main/resources/Homepage.png",
-    techStack: [
-      "React",
-      "Javascript",
-      "HTML",
-      "CSS",
-      "Socket.io",
-      "MongoDB",
-      "NodeJS",
-      "ExpressJS",
-    ],
-    time_from: "July 2023",
-    time_to: "Aug 2023",
-    description:
-      "Sharesketch is a collaborative real time whiteboad sharing website.",
-    liveDemo: "https://github.com/aslezar",
-  },
+	{
+		title: "Vetmedman",
+		img: "/vetmedman.jpg",
+		techStack: ["MongoDB", "NodeJS", "ExpressJS", "ReactJS"],
+		time_from: "Sep 2023",
+		time_to: "Feb 2024",
+		description:
+			"I developed Vetmedman's admin panel from the ground up using the MERN stack. This platform allows easy management of user data, articles, banners, orders, inquiries, and order statuses. It enhances user experience within the veterinary medical equipment and supplies sector..",
+		liveDemo: "https://www.vetmedman.com/",
+		// githubRepo: "https://github.com/aslezar/aslezar",
+	},
+	// {
+	//   title: "ShareSketch",
+	//   img: "https://github.com/aslezar/ShareSketch/raw/main/resources/Homepage.png",
+	//   techStack: [
+	//     "React",
+	//     "Javascript",
+	//     "HTML",
+	//     "CSS",
+	//     "Socket.io",
+	//     "MongoDB",
+	//     "NodeJS",
+	//     "ExpressJS",
+	//   ],
+	//   time_from: "July 2023",
+	//   time_to: "Aug 2023",
+	//   description:
+	//     "Sharesketch is a collaborative real time whiteboad sharing website.",
+	//   liveDemo: "https://github.com/aslezar",
+	// },
 ];
