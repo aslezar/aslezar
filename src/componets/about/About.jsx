0873e00593813b3dbf28828040390f234c2d239b
aslezar <97354675+aslezar@users.noreply.github.com>
2024-02-14 11:24:22 +0530

    Update text color in About component

--- src/componets/about/About.jsx
+++ src/componets/about/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
 						<h3 className="text-xl font-semibold mb-2">Education</h3>
 						<div className="shadow-md rounded-lg p-4">
 							<p className=" font-semibold mb-2">Bachelor of Technology</p>
-							<p className="text-gray-700 mb-2">
+							<p className="text-gray-200 mb-2">
 								Computer Science & Technology
 							</p>
 							<p className="text-gray-200 mb-2">
